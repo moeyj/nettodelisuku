5c564d7f5649378e549f47ea0e0465c030c014c0
moe <y_a_o_j_i@outlook.com>
2021-10-10 20:04:25 +0800

    1

---
+++ src/main/java/moe/yaoji/nettodelisuku/NettodelisukuApplication.java
@@ -0,0 +1,13 @@
+package moe.yaoji.nettodelisuku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettodelisukuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettodelisukuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/moe/yaoji/nettodelisuku/NettodelisukuApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.yaoji.nettodelisuku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettodelisukuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
