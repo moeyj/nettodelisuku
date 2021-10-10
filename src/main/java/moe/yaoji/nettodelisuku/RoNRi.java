73c5174294db9cff22f7e496b0590b4ab45cb8b4
moe <y_a_o_j_i@outlook.com>
2021-10-10 20:18:47 +0800

    2

---
+++ src/main/java/moe/yaoji/nettodelisuku/RoNRi.java
@@ -0,0 +1,7 @@
+package moe.yaoji.nettodelisuku;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoNRi {
+}
---
+++ src/main/java/moe/yaoji/nettodelisuku/SoUJiLYuU.java
@@ -0,0 +1,7 @@
+package moe.yaoji.nettodelisuku;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SoUJiLYuU {
+}
