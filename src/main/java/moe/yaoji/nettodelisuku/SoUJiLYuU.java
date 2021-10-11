2fc24ff02604f9e0cd78de2fe8e3a355927dd21f
moe <y_a_o_j_i@outlook.com>
2021-10-11 10:11:40 +0800

    6

--- src/main/java/moe/yaoji/nettodelisuku/SoUJiLYuU.java
+++ src/main/java/moe/yaoji/nettodelisuku/SoUJiLYuU.java
@@ -1,7 +1,9 @@
 package moe.yaoji.nettodelisuku;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -10,18 +12,20 @@ import java.io.IOException;
 
 @Controller
 public class SoUJiLYuU {
-    @PostMapping("/up")
-    public Integer f1(MultipartFile upfile){
+    @Value("${hulairu}")
+    String s;
+    @PostMapping("/upfile")
+    @ResponseBody
+    public int f1(MultipartFile upfile) {
         String originalFilename = upfile.getOriginalFilename();
-        File file = new File("D:/data/");
-        if (!file.exists()){
+        File file = new File(s);
+        if (!file.exists()) {
             file.mkdirs();//如果文件夹不存在则创建
         }
         try {
-            upfile.transferTo(new File(file+originalFilename));
+            upfile.transferTo(new File(s + originalFilename));
         } catch (IOException e) {
             e.printStackTrace();
-            return 1;
         }
         return 0;
     }
